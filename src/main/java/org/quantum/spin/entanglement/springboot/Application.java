22e79828f2b8ef1f8b837b4e424061eeb0995b7a
Dennis <4e65rv@gmail.com>
2020-07-26 15:39:51 +0900

    add: Junit Test file

--- src/main/java/org/quantum/spin/entanglement/springboot/Application.java
+++ src/main/java/org/quantum/spin/entanglement/springboot/Application.java
@@ -2,8 +2,11 @@ package org.quantum.spin.entanglement.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import web.HelloController;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = HelloController.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
