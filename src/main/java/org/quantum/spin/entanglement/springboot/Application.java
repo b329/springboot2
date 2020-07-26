36f59f7794526b9803a00cb66c3514ee074d2fb7
Dennis <4e65rv@gmail.com>
2020-07-26 15:49:12 +0900

    add: Component Scan Test

--- src/main/java/org/quantum/spin/entanglement/springboot/Application.java
+++ src/main/java/org/quantum/spin/entanglement/springboot/Application.java
@@ -1,12 +1,16 @@
 package org.quantum.spin.entanglement.springboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import web.HelloController;
 
 @SpringBootApplication
 @ComponentScan(basePackageClasses = HelloController.class)
+//@ComponentScan(basePackages = "org.quantum.spin.entanglement.springboot")
+//@EnableAutoConfiguration
+//@ComponentScan
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
