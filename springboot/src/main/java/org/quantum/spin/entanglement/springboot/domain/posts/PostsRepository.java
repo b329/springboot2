aec641ed5fce9c10cca4a304a1826252134efe89
Dennis <4e65rv@gmail.com>
2020-08-04 21:49:08 +0900

    fix: file packages. including deleted files

--- springboot/src/main/java/org/quantum/spin/entanglement/springboot/domain/posts/PostsRepository.java
+++
@@ -1,6 +0,0 @@
-package org.quantum.spin.entanglement.springboot.domain.posts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostsRepository extends JpaRepository<Posts, Long> {
-}
