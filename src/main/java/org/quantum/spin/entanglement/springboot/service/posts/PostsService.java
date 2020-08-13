405154c6cdd0d355be20d94eb9674e85cc933bfb
나영철 <4e65rv@feelway.com>
2020-08-13 09:33:42 +0900

    add: javadoc 형식으로 주석 변경

--- src/main/java/org/quantum/spin/entanglement/springboot/service/posts/PostsService.java
+++ src/main/java/org/quantum/spin/entanglement/springboot/service/posts/PostsService.java
@@ -11,7 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 /**
 @Autowired 가 없는 이유는 빈을 주입받는 방식에
-@Autowired setter 생성자 주 방식이 있는데
+@Autowired setter 생성자 주입방식이 있는데
 여기서는 Controller 와 Service 에서 lombok 의 @RequiredArgsConstructor 로 생성자 Bean 을 주입받기 때문에
 Autowired 가 없다.
 생성자를 직접 안쓰고 lombok 을 사용하는 이점은 해당 클래스의 의존성 관계가 변경될때마다 생성자코드를 계속해서 수정해야 하는 번거로움을
--- src/main/java/org/quantum/spin/entanglement/springboot/web/PostsApiController.java
+++ src/main/java/org/quantum/spin/entanglement/springboot/web/PostsApiController.java
@@ -10,12 +10,13 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RestController
 
-// @Autowired 가 없는 이유는 빈을 주입받는 방식에
-// @Autowired, setter, 생성자 주압 방식이 있는데
-// 여기서는 Controller 와 Service 에서 lombok 의 @RequiredArgsConstructor 로 생성자 Bean 을 주입받기 때문에
-// Autowired 가 없다.
-// 생성자를 직접 안쓰고 lombok 을 사용하는 이점은 해당 클래스의 의존성 관계가 변경될때마다 생성자코드를 계속해서 수정해야 하는 번거로움을
-// 피하기 위해서이다.
+/** @Autowired 가 없는 이유는 빈을 주입받는 방식에
+@Autowired, setter, 생성자 주압 방식이 있는데
+여기서는 Controller 와 Service 에서 lombok 의 @RequiredArgsConstructor 로 생성자 Bean 을 주입받기 때문에
+Autowired 가 없다.
+생성자를 직접 안쓰고 lombok 을 사용하는 이점은 해당 클래스의 의존성 관계가 변경될때마다 생성자코드를 계속해서 수정해야 하는 번거로움을
+피하기 위해서이다.
+*/
 
 public class PostsApiController {
 
